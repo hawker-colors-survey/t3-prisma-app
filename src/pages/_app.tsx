1778a66120439361be5f7b696434865a0416b3a5
Shaun Tung <shaun.tung@gmail.com>
2023-05-08 02:54:54 +0800

    basic setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,55 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
+import Head from "next/head";
 import { SessionProvider } from "next-auth/react";
+import { MantineProvider } from "@mantine/core";
 
 import { api } from "~/src/utils/api";
-
-import "~/src/styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <title>Hawker Colours</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <SessionProvider session={session}>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            colorScheme: "light",
+            fontFamily: "Lora, serif",
+            headings: { fontFamily: "Ubuntu, sans-serif" },
+            fontSizes: {
+              xs: "15px",
+              sm: "18px",
+              md: "18px",
+              lg: "32px",
+              xl: "72px",
+            },
+            components: {
+              Title: {
+                styles: {
+                  root: {
+                    "&:is(h1)": { fontWeight: 800 },
+                    "&:is(h2)": { fontSize: 24, fontWeight: 500 },
+                  },
+                },
+              },
+            },
+          }}
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+      </SessionProvider>
+    </>
   );
 };
 
